358d2ad2f0d0af6b92178e2854844ad5d7cbaf4c
CelestialCrafter <celestialcraftermc@gmail.com>
2025-01-28 15:29:26 -0600

    f-curve based hashing function

--- script.py
+++ script.py
@@ -6,15 +6,44 @@ import sys
 import bpy
 
 
-def hash_frame(objects):
-    hashes = [
-        "l1{:f}-l2{:f}-l3{:f}-r1{:f}-r2{:f}-r3{:f}-s1{:f}-s2{:f}-s3{:f}".format(
-            *object.location, *object.rotation_euler, *object.scale
-        )
-        for object in objects
-    ]
+def hash_frame(scene):
+    slopes = []
 
-    return "".join(hashes)
+    def process(value):
+        if not (
+            hasattr(value, "animation_data")
+            and value.animation_data
+            and value.animation_data.action
+        ):
+            return
+
+        for fcurve in value.animation_data.action.fcurves:
+            frame = scene.frame_current
+            current = fcurve.evaluate(frame)
+            next = fcurve.evaluate(frame + 1)
+            slopes.append(next - current)
+
+    for object in scene.objects:
+        # object animations
+        process(object)
+
+        # modifier animations
+        for modifier in object.modifiers:
+            process(modifier)
+
+        for slot in object.material_slots:
+            if not slot.material:
+                continue
+            material = slot.material
+
+            # material animations
+            process(material)
+
+            # node tree animations
+            if material.use_nodes:
+                process(material.node_tree)
+
+    return " ".join([str(slope) for slope in slopes])
 
 
 def render(base_path, scene):
@@ -22,7 +51,7 @@ def render(base_path, scene):
     for i in range(scene.frame_start, scene.frame_end):
         scene.frame_set(i)
         current_path = os.path.join(base_path, f"images/{i}.png")
-        current_hash = hash_frame(scene.objects.values())
+        current_hash = hash_frame(scene)
 
         if prev[0] != current_hash:
             prev = (current_hash, i)
